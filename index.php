8b6297ffb3337e6e060bdbe680f832b2797c0c9b
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-19 09:37:58 +0100

    exo1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $int = 0;
+    while ($int < 10) {
+      echo "$int";
+      $int++;
+    }
+    ?>
+  </body>
